fed9f6bd67a3697f2e7f143d99761508c8ee414a
Dean Friedland <dfmmalaw@gmail.com>
2016-07-31 12:41:20 -0400

    added calculation methods

--- BegC-chap1-consoleapp/Program.cs
+++ BegC-chap1-consoleapp/Program.cs
@@ -8,8 +8,21 @@ namespace BegC_chap1_consoleapp
 {
     class Program
     {
+        static void MultiplyTwoNumbers (int a, int b)
+        {
+            int product = a * b;
+            Console.WriteLine(product);
+        }
+        static void AddTwoNumbers (int c, int d)
+        {
+            int sum = c + d;
+            Console.WriteLine(sum);
+        }
         static void Main(string[] args)
         {
+            MultiplyTwoNumbers(2, 3);
+            AddTwoNumbers(4, 5);
+            Console.ReadLine();
         }
     }
 }
