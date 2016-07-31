e02765d41194bc233d5c8b1d610fb49543015715
Dean Friedland <dfmmalaw@gmail.com>
2016-07-31 12:22:48 -0400

    initial commit

---
+++ BegC-chap1-consoleapp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BegC_chap1_consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
